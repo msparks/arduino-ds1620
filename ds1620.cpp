f2466e8da4de89ade14261bc254421528bb90896
Matt Sparks <ms@quadpoint.org>
2011-01-13 21:30:49 -0800

    Whitespace

--- ds1620.cpp
+++ ds1620.cpp
@@ -1,25 +1,23 @@
 /*
   DS160 Library
- 
+
   by Ruben Laguna 2009 <ruben.laguna at gmail.com>
 
   based on examples from Tom Tigoe <http://www.arduino.cc/en/Reference/SoftwareSerialExample>
   and phanderson <http://www.phanderson.com/printer/ds1620/ds1620.html>
-  
-  
+
   written: 30 Aug 2008
- 
 */
 
 #include "WProgram.h"
 #include "ds1620.h"
+
 
 Ds1620::Ds1620(int rst, int clk, int dq):rstPin(rst),clkPin(clk),dqPin(dq)
 {
   pinMode(rst, OUTPUT);
   pinMode(clk, OUTPUT);
   pinMode(dq, OUTPUT);
-
 }
 
 void Ds1620::config()
@@ -27,30 +25,28 @@ void Ds1620::config()
 
   // write configuration register in DS1620
   rst_low();
-    
   clk_high();
   rst_high(); //all data transfer are initiated by driving RST high
   write_command(0x0c); // write config command
   write_command(0x03); // cpu mode + 1 SHOT
   rst_low();
-  delay(200); //wait until the configuration register is written 
-
+  delay(200); //wait until the configuration register is written
 }
 
 
-void Ds1620::start_conv() 
+void Ds1620::start_conv()
 {
-  // START_CONV 
+  // START_CONV
   clk_high();
   rst_high();
   write_command(0xEE); //start conversion
   rst_low();
-  delay(700);  
+  delay(700);
 }
 
-void Ds1620::stop_conv() 
+void Ds1620::stop_conv()
 {
-   // STOP_CONV  
+  // STOP_CONV
   clk_high();
   rst_high();
   write_command(0x22); //stop conversion
@@ -59,10 +55,10 @@ void Ds1620::stop_conv()
 }
 
 void Ds1620::write_command(int command)
-/* sends 8 bit command on DQ output, least sig bit first */ 
+/* sends 8 bit command on DQ output, least sig bit first */
 {
   int n, bit;
-  
+
   for(n=0;n<8;n++)
   {
     bit = ((command >> n) & (0x01));
@@ -87,8 +83,8 @@ int Ds1620::read_raw_data(void)
   int raw_data=0;
 
   pinMode(dqPin,INPUT);
-  
-     /* jam the dq lead high to use as input */
+
+  /* jam the dq lead high to use as input */
   for(n=0;n<9;n++)
   {
      clk_low();
@@ -100,10 +96,10 @@ int Ds1620::read_raw_data(void)
   return(raw_data);
 }
 
-void Ds1620::out_bit(int bit) 
+void Ds1620::out_bit(int bit)
 {
   digitalWrite(dqPin, bit);  /* set up the data */
-  clk_low();             /* and then provide a clock pulse */   
+  clk_low();             /* and then provide a clock pulse */
   clk_high();
 }
 
@@ -119,10 +115,10 @@ void Ds1620::clk_low(void)
 
 void Ds1620::rst_high(void)
 {
-   digitalWrite(rstPin,HIGH);
+  digitalWrite(rstPin,HIGH);
 }
 
 void Ds1620::rst_low(void)
 {
-   digitalWrite(rstPin,LOW);
+  digitalWrite(rstPin,LOW);
 }
--- ds1620.h
+++ ds1620.h
@@ -1,41 +1,35 @@
 /*
   DS160 Library
- 
+
   by Ruben Laguna 2009 <ruben.laguna at gmail.com>
   based on examples from Tom Tigoe <http://www.arduino.cc/en/Reference/SoftwareSerialExample>
   and phanderson <http://www.phanderson.com/printer/ds1620/ds1620.html>
-  
-  
+
   written: 30 Aug 2008
- 
- 
 */
 #ifndef ds1620_h
 #define ds1620_h
 
-#include <WConstants.h> 
+#include <WConstants.h>
 
 class Ds1620
 {
-  public:
-    Ds1620(int rst, int clk, int dq);
-    void config();
-    void start_conv() ;
-    void stop_conv() ;
-    int read_data();
-  
-  private:
-    int rstPin, clkPin, dqPin; 
-    int read_raw_data(void);
-    void write_command(int command);
-    void out_bit(int bit) ;
-    void clk_high(void);
-    void clk_low(void);
-    void rst_high(void);
-    void rst_low(void);
-  
+ public:
+  Ds1620(int rst, int clk, int dq);
+  void config();
+  void start_conv();
+  void stop_conv();
+  int read_data();
+
+ private:
+  int rstPin, clkPin, dqPin;
+  int read_raw_data(void);
+  void write_command(int command);
+  void out_bit(int bit);
+  void clk_high(void);
+  void clk_low(void);
+  void rst_high(void);
+  void rst_low(void);
 };
-
-
 
 #endif
